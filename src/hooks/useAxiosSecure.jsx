46e4da6bede11b2c31f0a30b52324fd9ce36ec94
romanakhatun <romanakhatun2004@gmail.com>
2025-12-18 10:36:02 +0600

    change backend url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://textilia-garments-server.vercel.app",
 });
 
 const useAxiosSecure = () => {
